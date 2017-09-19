19b8395530fbe01a39964e8ad9ff54687e0b60d1
its-suun <suun@teknik.io>
2017-09-19 16:43:15 +0200

    Add more starfall methods

--- lua/csm/misc.lua
+++ lua/csm/misc.lua
@@ -13,12 +13,36 @@ function csm.starfall_processor:quotaAverage()
 	return self.instance.cpu_average
 end
 
-function csm.starfall_processor:quotaMax ()
+function csm.starfall_processor:quotaMax()
 	return self.instance.cpuQuota
+end
+
+function csm.starfall_processor:getState()
+	local state = self:GetNWInt("State")
+	
+	if state == self.States.Normal then
+		return "Normal"
+	elseif state == self.States.Error then
+		return "Error"
+	else
+		return "None"
+	end
+end
+
+function csm.starfall_processor:getError()
+	if self.error and self.error.message then
+		return self.error.message
+	end
 end
 
 if CLIENT then
 	function csm.starfall_processor:getName()
 		return self.name
+	end
+else
+	function csm.starfall_screen:getLinked()
+		if self.link then
+			return SF.Entities.Wrap(self.link)
+		end
 	end
 end
